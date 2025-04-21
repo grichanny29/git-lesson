a228682f705003d886b40af1a38800bd61593f7e
alex <alex.grich84@gmail.com>
2025-04-21 11:12:06 +0300

    урок 55 пагинация

--- src/componets/Users/UsersContainer.jsx
+++ src/componets/Users/UsersContainer.jsx
@@ -1,11 +1,48 @@
 import React from "react";
-import Users from "./Users";
 import { connect } from "react-redux";
-import { followAC, setUsersAC, unfollowAC } from "../../redux/users-reduser";
+import { followAC, setUsersAC, unfollowAC,setCurrentPageAC,setUsersTotalCountAC} from "../../redux/users-reduser";
+import axios from "axios";
+import Users from "./Users";
+
+
+
+class UsersContainer extends React.Component {
+	componentDidMount () {
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).
+				 then (response => {
+						this.props.setUsers (response.data.items)
+						this.props.setTotalUsersCount(response.data.totalCount);
+	});
+	}
+	
+		 onPageChanged = (pageNumber) => {
+					this.props.setCurrentPage(pageNumber);
+					axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).
+					then (response => {
+						 this.props.setUsers (response.data.items);
+	 });
+		 }
+	
+		render () {
+				return <Users totalUsersCount={this.props.totalUsersCount} 
+											pageSize={this.props.pageSize}
+											currentPage={this.props.currentPage}	
+											onPageChanged={this.onPageChanged}
+											users ={this.props.users}
+											follow ={this.props.follow}
+											unfollow ={this.props.unfollow}
+											/>
+	}
+		}
+	
+	
 
 let mapStateToProps = (state) => {
 	return {
-		 users: state.usersPage.users
+		 users: state.usersPage.users,
+		 pageSize: state.usersPage.pageSize,
+		 totalUsersCount:state.usersPage.totalUsersCount,
+		 currentPage:state.usersPage.currentPage
 	}
 }
 
@@ -19,9 +56,14 @@ let mapDispatchToProps = (dispatch) => {
 		},
 		setUsers: (users) => {
 			dispatch(setUsersAC(users));
-		}
+		},
+		setCurrentPage: (pageNumber) => {
+			dispatch( setCurrentPageAC(pageNumber));
+		},
+		  setTotalUsersCount: (totalCount) => {
+			dispatch(setUsersTotalCountAC(totalCount));
 	}
 }
+}
 
-
-export default connect(mapStateToProps,mapDispatchToProps) (Users);
+export default connect(mapStateToProps,mapDispatchToProps) ( UsersContainer );
